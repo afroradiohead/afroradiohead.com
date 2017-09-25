414246b5fd34bf6dfc667f141fbf77e8c308fd2f
Christopher Clarke <afroradiohead@gmail.com>
2017-09-25 13:01:11 -0700

    reconfigure serving of app

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { Afroradiohead.ComPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('afroradiohead.com App', () => {
-  let page: Afroradiohead.ComPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new Afroradiohead.ComPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ElementsBoardGameComponent } from './elements-board-game/elements-board-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ElementsBoardGameComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
